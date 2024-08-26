ec3618f39021e28758066f5e5c2061f9d478256e
Alexey Kuzko <105299483+AlexeyKuzko@users.noreply.github.com>
2024-08-26 17:44:26 +0300

    add echobot

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # OnlyImportantNewsBot
 for Telegram.
+
+git clone https://github.com/AlexeyKuzko/OnlyImportantNewsBot
+cd OnlyImportantNewsBot
+python3 -m venv /venv
+source venv/bin/activate
+pip install -r requirements.txt
+nohup ~/bin/python ~/bot.py
--- bot.py
+++ bot.py
@@ -1,1 +1,21 @@
-#it will be here
+import telebot
+
+TOKEN = 'TOKEN'
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['help', 'start'])
+def welcome(message):
+    bot.reply_to(message, """\
+Hi there, I am OnlyImportantNewsBot.
+At this moment I can only echo messages. What do you want me to echo?\
+""")
+
+
+@bot.message_handler(func=lambda message: True)
+def echo(message):
+    bot.reply_to(message, message.text)
+
+
+bot.infinity_polling()
